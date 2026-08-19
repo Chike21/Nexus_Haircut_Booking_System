916eef0e075b7d77550d8b316ef190142db10b77
Chike <chikezirimjustice@gmail.com>
2026-08-19 15:08:10 +1300

    Fix allowed hosts for PythonAnywhere

--- booking_project/settings.py
+++ booking_project/settings.py
@@ -42,7 +42,7 @@ DEBUG = os.environ.get("DEBUG", "False") == "True"
 ALLOWED_HOSTS = [
     "localhost",
     "127.0.0.1",
-    "chikezirimharry.pythonanywhere.com",
+    "chikezirimjustice.pythonanywhere.com",
 ]
 
 
